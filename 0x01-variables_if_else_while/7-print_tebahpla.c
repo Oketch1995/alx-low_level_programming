3e62f072c57a23d1be0389949bb5534c33e28f41
Oketch1995 <velmaatieno4009@gmail.com>
2022-09-12 07:48:40 -0700

    task seven

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main -entry point
+ *
+ * description: lowercase alphabet in reverse
+ *
+ * return: always 0 (success)
+ */
+int main(void)
+{
+char s;
+for (s = 'z'; s >= 'a'; s--)
+{
+putchar(s);
+}
+putchar('\n');
+return (0);
+}
