e1239c0e77247eec88b2acda0f82b4c9bf157f50
Oketch1995 <velmaatieno4009@gmail.com>
2022-09-13 03:08:48 -0700

    task one

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,12 +1,15 @@
 #include "main.h"
 /**
- * main - test for a postitive_or_negative() function 
- * return : 0
- */
+ *
+ * * main - tests function that prints if integer is positive or negative
+ *
+ * * Return: 0
+ *
+ * */
 int main(void)
 {
 int i;
-i = 0;
+i = 98;
 positive_or_negative(i);
 return (0);
 }
