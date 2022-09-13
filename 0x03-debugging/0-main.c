fb8aaf157ac36960a3033ce6bf99edc7bd159c54
Oketch1995 <velmaatieno4009@gmail.com>
2022-09-13 04:18:07 -0700

    task 1

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,13 +1,15 @@
 #include "main.h"
 
 /**
- * main - prints if integer is positive or negative
+ * main - print positive or negative
  * Return: Always 0 (success)
  */
 int main(void)
 {
 int i;
+
 i = 0;
 positive_or_negative(i);
+
 return (0);
 }
