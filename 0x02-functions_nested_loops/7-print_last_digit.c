5331f530dded2b235d6384b17414f3e832dd670c
Oketch1995 <velmaatieno4009@gmail.com>
2022-09-14 07:48:48 -0700

    task 7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
  *print_last_digit - print the last digit of a number
- *@n: the number in question
+ *@n: The number in question
  *
- *Return: value of the last digit
+ *Return: Value of the last digit
  */
 int print_lst_digit(int n)
 {
