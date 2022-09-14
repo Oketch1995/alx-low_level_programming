89fe88a258b82b423b7e6f8ec64f2f680fd4f298
Oketch1995 <velmaatieno4009@gmail.com>
2022-09-14 01:31:35 -0700

    task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,15 +2,14 @@
 /**
  * main - print_alphabets - function to print abc
  *
- * Return: 0
+ * Return: Always 0 (success)
  */
 void print_alphabet(void)
 {
 char c;
 for (c = 'a'; c <= 'z'; c++)
 {
-_putchar(char);
+_putchar(char 'c');
 }
-_putchar('\n');
-return (0);
+_putchar("\n");
 }
