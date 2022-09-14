32e502b8a8d7d6e1bdcdd7395ffaa5bdde8c0d9c
Oketch1995 <velmaatieno4009@gmail.com>
2022-09-14 10:24:34 -0700

    task 1

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,9 +7,9 @@
  */
 int main(void)
 {
-_putchar("_");
-_putchar("p");
-_putchar("u");
+_putchar(char * "_");
+_putchar(char * "p");
+_putchar(char * "-");
 _putchar("t");
 _putchar("c");
 _putchar("h");
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,7 +1,7 @@
 #ifndef HOLBERTON_H_INCLUDED
 #define HOLBERTON_H_INCLUDED
 
-int _putchar(char);
+int _putchar(char *);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
@@ -9,7 +9,7 @@ int _isalpha(int c);
 int print_sign(int n);
 int _abs(int);
 int print_last_digit(int);
-void jack_bauer(void);
+void jack_biauer(void);
 void time_table(void);
 int add(int a, int b);
 void print_to_98(int n);
