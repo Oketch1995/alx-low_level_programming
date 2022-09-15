5259aa62c0a604a20a6d5e6fbab8d17d289fe3b3
Oketch1995 <velmaatieno4009@gmail.com>
2022-09-15 03:30:03 -0700

    task 3

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -6,7 +6,7 @@
 
 void positive_or_negative(int i);
 int largest_number(int a, int b, int c);
-void print_remaining_days(int, int, int);
+void print_remaining_days(int month, int day, int year);
 int convert_days(int, int);
 
 #endif /* MAIN.H*/
