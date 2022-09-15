9e6f2ee85ee71154b3a2a975797d376afd36d539
Oketch1995 <velmaatieno4009@gmail.com>
2022-09-15 01:09:46 -0700

    yask 0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,9 +1,9 @@
 #include"main.h"
 /**
- *positive_or_negative -prints if integer is negatibve or positive
+ *main -prints if integer is negatibve or positive
  * Return: 0
  */
-void positive_or_negative(int i)
+int main(void);
 {int i;
 i = 0;
 positive_or_negative(i);
