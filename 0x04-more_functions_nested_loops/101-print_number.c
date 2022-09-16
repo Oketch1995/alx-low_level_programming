a408edbe64e072dbf13c60c12cea65097323dc55
Oketch1995 <velmaatieno4009@gmail.com>
2022-09-16 08:02:10 -0700

    final task

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -8,16 +8,15 @@
 
 void print_number(int n)
 {
-unsigned int num = n;
+	unsigned int y;
 
-if (n < 0)
-{
-putchar('-'};
-num = -num;
-}
-if (num > 9)
-{
-print_number(num / 10);
-}
-putchar(num % 10 + '0');
+	y = n;
+	if (n < 0)
+	{
+		_putchar(45);
+		y = -n;
+	}
+	if (y / 10)
+		print_number(y / 10);
+	_putchar((y % 10) + '0');
 }
