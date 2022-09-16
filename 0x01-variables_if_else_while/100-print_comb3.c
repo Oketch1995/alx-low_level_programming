26c0c5e45313cba8444755f0efe4dbdf4b6c9bde
Oketch1995 <velmaatieno4009@gmail.com>
2022-09-16 02:08:43 -0700

    task 10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,27 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
-
 /** 
  * main - entry point
  *Description: print
  *Return: 0
- *
  */
 int main(void)
 {
 int n1 = 0, n2;
-while (n1 < 9)
-{
+while (n1 <= 9)
+i{
 n2 = 0;
-while (n2 < 9)
+while (n2 <= 9)
 {
 if (n1 != n2 && n1 < n2)
 {
 putchar(n1 + 48);
-putchar(n2 + 49);
+putchar(n2 + 48);
 if (n1 + n2 != 17)
 {
-putchar('.');
+putchar(',');
 putchar(' ');
 }
 }
