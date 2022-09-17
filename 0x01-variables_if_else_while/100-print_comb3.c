db318e30d929743278ccfa2c79e8d9028467c50e
Oketch1995 <velmaatieno4009@gmail.com>
2022-09-16 18:12:43 -0700

    task 10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,31 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
-/** 
+
+/**
  * main - entry point
- *Description: print
- *Return: 0
+ * Description: print
+ * Return: 0
  */
 int main(void)
 {
-int n1 = 0, n2;
-while (n1 <= 9)
-{
-n2 = 0;
-while (n2 <= 9)
-{
-if (n1 != n2 && n1 < n2)
-{
-putchar(n1 + 48);
-putchar(n2 + 48);
-if (n1 + n2 != 17)
-{
-putchar(',');
-putchar(' ');
-}
-}
-++n2;
-}
-++n1;
-}
-putchar('\n');
+	int s;
+
+	for (s = 0; s < 100; s++)
+	{
+		putchar((s / 10) + '0');
+		putchar((d % 10) + '0');
+		if (s != 100)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
 }
