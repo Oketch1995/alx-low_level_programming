487276532f9558f5308e9174e1197ccc5e196d4e
Oketch1995 <velmaatieno4009@gmail.com>
2022-09-18 08:03:35 -0700

    task 11

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -21,7 +21,7 @@ int main(void)
 				putchar((a % 10) + '0');
 				putchar((b % 10) + '0');
 				putchar((c % 10) + '0');
-				if (a -- 7 && b == 8 && c == 9)
+				if (a == 7 && b == 8 && c == 9)
 					continue;
 				putchar(',');
 				putchar(' ');
@@ -31,7 +31,3 @@ int main(void)
 	putchar('\n');
 	return (0);
 }
-
-
-
-
