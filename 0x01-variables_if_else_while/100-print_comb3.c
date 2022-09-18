b209e4d664278d15426d236e9bfb50c23b4bcebd
Oketch1995 <velmaatieno4009@gmail.com>
2022-09-18 05:00:06 -0700

    task 10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -20,17 +20,14 @@ int main(void)
 			putchar((x % 10) + '0');
 			putchar(' ');
 			putchar((y / 10) + '0');
-			putchar((y % 10) + '0';
+			putchar((y % 10) + '0');
 					if (x == 98 & y == 99)
 					continue;
 					putchar(',');
 					putchar(' ');
-         	}
-	}
-					putchar('\n');
-					return (0);
+		}
+	putchar('\n');
+	return (0);
 }
-					
-				
 
 
