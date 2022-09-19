d526be2d612f3a19ba699d8bb490d1e41d6cbe4c
Oketch1995 <velmaatieno4009@gmail.com>
2022-09-19 12:23:36 -0700

    task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * print_rev - print a string in reverse
+ * @s: spring to reverse
+ * Return: nothing
+ */
+void print_rev(char *s)
+{
+	int len = strlen(s);
+
+	while(len--)
+		putchar(*(s + len));
+	putchar(10);
+}
