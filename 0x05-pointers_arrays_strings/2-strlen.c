35344b271b9bec71887a196a72979d45d90f6066
Oketch1995 <velmaatieno4009@gmail.com>
2022-09-19 08:10:02 -0700

    task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _strlen - Write a function that returns the length of a string
+ * @s: parameter
+ * Return: returns length as an integer
+ */
+int _strlen(char *s)
+{
+	int len = 0;
+	while (*(s + len) != '\0')
+		len++;
+	return (len);
+}
