88c3da79e547487b1e6682a8364b1206cafd6c5d
Oketch1995 <velmaatieno4009@gmail.com>
2022-09-19 13:33:33 -0700

    task 6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -2,8 +2,9 @@
 #include "main.h"
 
 /**
- * puts2 - prints every other character of a string
+ * puts2 - prints every other character
  * @str - string
+ *
  * Return: nothing
  */
 void puts2(char *str)
