79415ce48c7902da95416c91babe3b58ad064cd9
Oketch1995 <velmaatieno4009@gmail.com>
2022-09-22 12:36:37 -0700

    task 3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strcmp - Write a function that compares two strings
+ * @s1: pointer to the first string
+ * @s2: pointer to the second string
+ * Return:
+ * Return: Always 0
+ */
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1 && *s2 && *s1 == *s2)
+	{
+		s1++;
+		s2++;
+	}
+	return (*s1 - *s2);
+}
+
