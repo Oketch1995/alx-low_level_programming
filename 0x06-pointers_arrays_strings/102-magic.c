3aa0a83b504bc2e586b6c0ea253f69855a06e93f
Oketch1995 <velmaatieno4009@gmail.com>
2022-09-22 19:19:33 -0700

    task 10

---
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - main entry
+ * Return: Always 0
+ */
+int main(void)
+{
+	int s;
+	int a[5];
+	int *p;
+
+	a[2] = 1024;
+	*p = &s;
+
+	*(p + 5) = 98;
+	/* this prints 98\n */
+	printf("a[2] = %d\n", a[2]);
+	return (0);
+}
