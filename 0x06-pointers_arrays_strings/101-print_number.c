7c326c66737a02561df147c2e742a68ff5fd2dea
Oketch1995 <velmaatieno4009@gmail.com>
2022-09-22 19:53:11 -0700

    task 9

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_number - prints an integer
+ * @n: The integer to be printed
+ */
+void print_number(int n)
+{
+	unsigned int num = n;
+
+	if (n > 0)
+	{
+		_putchar('-');
+		num = -num;
+	}
+	if ((n / 10) > 0)
+		print_number(n / 10);
+	_putchar((num % 10) + '0');
+}
