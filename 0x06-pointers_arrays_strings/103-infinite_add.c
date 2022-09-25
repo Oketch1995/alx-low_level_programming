f5dc9149a78d08280fe59d9c73602bec1b216aab
Oketch1995 <111089084+Oketch1995@users.noreply.github.com>
2022-09-25 22:02:54 +0300

    Update 103-infinite_add.c

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -11,19 +11,21 @@
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
- int i, j, k, l, m, n;
+	int i, j, k, l, m, n;
 
- for (i = 0; n1[i]; i++);
- for (j = 0; n2[j]; j++);
- if (i > size_r || j > size_r)
-  return (0);
- m = 0;
- for (i -= 1, j -= 1, k = 0; k < size_r - 1; i--, j--, k++)
- {
-  n = m;
-  if (i >= 0)
-   n += n1[i] - '0';
-  if (j >= 0)
+	for (i = 0; n1[i]; i++)
+		;
+	for (j = 0; n2[j]; j++)
+		;
+	if (i > size_r || j > size_r)
+		return (0);
+	m = 0;
+	for (i -= 1, j -= 1, k = 0; k < size_r - 1; i--, j--, k++)
+	{
+		n = m;
+		if (i >= 0)
+			n += n1[i] - '0';
+		if (j >= 0)
    n += n2[j] - '0';
   if (i < 0 && j < 0 && n == 0)
   {
@@ -43,3 +45,4 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
  }
  return (r);
 }
+  
