b1e9f583d00b45499630a4d3f48a60d77740da80
Oketch1995 <111089084+Oketch1995@users.noreply.github.com>
2022-09-25 22:12:33 +0300

    Update 101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -8,16 +8,16 @@
 
 void print_number(int n)
 {
-	unsigned int i = n;
+	unsigned int k = n;
 
 	if (n < 0)
 	{
 		_putchar(45);
-		i = -i;
+		k = -k;
 	}
-	if (i / 10)
+	if (k / 10)
 	{
-		print_number(i / 10);
+		print_number(k / 10);
 	}
-	_putchar(i % 10 + '0');
+	_putchar(k % 10 + '0');
 }
