6f1751710c0e0334977cf67b9dbe8be42d6e0a4a
Oketch1995 <velmaatieno4009@gmail.com>
2022-09-25 06:59:23 -0700

    task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,9 +1,34 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * _strlen-return the length of a string
- * @s: The string to be capitalised
- * Return: The string
+ * cap_string - function that capitalize th first character of the word
+ * @str: function to be capitalised
+ * Return: Return the capitalised string
  */
- 
+char *cap_string(char *str)
+{
+	int byt = 0;
+
+	while (str[++byt])
+	{
+		(!(str[byt] >= 'a' && str[byt] <= 'z'))
+			byt++;
+		if (str[byt - 1] == ' ' ||
+				str[byt - 1] == '\t' ||
+				str[byt - 1] == '\n' ||
+				str[byt - 1] == ',' ||
+				str[byt - 1] == ';' ||
+				str[byt - 1] == '.' ||
+				str[byt - 1] == '!' ||
+				str[byt - 1] == '?' ||
+				str[byt - 1] == '"' ||
+				str[byt - 1] == '(' ||
+				str[byt - 1] == ')' ||
+				str[byt - 1] == '{' ||
+				str[byt - 1] == '}')
+			str[byt] -= 32;
+	}
+	return (str);
+}
+
+
