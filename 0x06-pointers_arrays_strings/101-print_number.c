775b372fa16b79186a36c27b7e7c99286c25fb85
Oketch1995 <velmaatieno4009@gmail.com>
2022-09-25 11:13:54 -0700

    task 9

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -7,15 +7,15 @@
  */
 void print_number(int n)
 {
-unsigned int i = n;
-if (n < 0)
-{
-_putchar(45);
-i = -i;
-}
-if (i / 10)
-{
-print_number(i / 10);
-}
-_putchar(i % 10 + '0');
+	unsigned int i = n;
+	if (n < 0)
+	{
+		_putchar(45);
+		i = -i;
+	}
+	if (i / 10)
+	{
+		print_number(i / 10);
+	}
+	_putchar(i % 10 + '0');
 }
