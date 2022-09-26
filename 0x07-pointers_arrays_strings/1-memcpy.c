713b85739f5a1a83449f08f6afdfe885572d1e6d
Oketch1995 <velmaatieno4009@gmail.com>
2022-09-26 07:38:21 -0700

    task 1

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _memcpy - copy memiory area
+ * @dest: dest memory area
+ * @src: source memory area
+ * @n: bytes from memory area to copy
+ * Return: memory area replaced.
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int k;
+
+	for (k = 0; k < n; k++)
+	{
+		dest[k] = src[k]
+	}
+	return (dest);
+}
