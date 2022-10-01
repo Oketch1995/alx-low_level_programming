79990ebe0ab822e28988cb613ec757da1779d1f0
Oketch1995 <velmaatieno4009@gmail.com>
2022-09-30 18:49:38 -0700

    task 1

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,10 +1,18 @@
 #include "main.h"
 #include <stdio.h>
 
+
 /**
 * main - Prints its name
 * @argc: Arguments count
 * @argv: Argument 
 *
-* Return: Always 0
+* Return: Always 0(success)
 */
+int main(int argc, char *argv[])
+{
+
+	(void) argc;
+	printf("%i\n", argc - 1);
+	return (0);
+}
