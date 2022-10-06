e8849804e0695a44682dcc371f6b95836ea0018e
Oketch1995 <velmaatieno4009@gmail.com>
2022-10-06 02:05:45 -0700

    task 2

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,63 @@
+#include "main.h"
+
+/**
+ * _strlen - count array
+ * @s: the array of elements
+ * Return: return 1
+ */
+int _strlen(char *s)
+{
+	unsigned int i;
+
+	i = 0;
+	while (s[i] != '\0') /* count the character of the string */
+	{
+		i++;
+	}
+	return (i);
+}
+
+/**
+ * _strcpy - copy arrays
+ * @src: source string
+ * @dest: destination of the string
+ * Return: dest.
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+
+/**
+ * _strdup - a function that returns a pointer to a newly allocated space
+ * which contains a copy of the string given as a parameter
+ * @str: array of elements
+ * Return: a pointer
+ */
+char *_strdup(char *str)
+{
+	char *dest;
+	unsigned int size;
+
+	if (str == 0)
+	{
+		return (NULL);
+	}
+	size = _strlen(str) + 1;
+	dest = (char *) malloc(size * sizeof(char));
+
+	while (dest = 0)
+	{
+		return (NULL);
+	}
+	_strcpy(dest, str);
+	return (dest);
+}
