dbd41a5d378d606edb09924b200ddb39437d5f7f
Oketch1995 <111089084+Oketch1995@users.noreply.github.com>
2022-10-06 12:18:58 +0300

    Update 1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,65 +1,28 @@
 #include "main.h"
-#include <stddef.h>
-#include <stdlib.h>
-
 /**
- * _strlen - count array
- * @s: the array of elements
- * Return: return 1
+ * _strdup - a function that returns a pointer to a newly allocated
+ * space in memory, which contains a copy of the string given as a parameter
+ * @str: string to be duplicated
+ * Return: NULL if str = NULL, pointer to the duplicated string
  */
-int _strlen(char *s)
-{
-	unsigned int i;
 
-	i = 0;
-	while (s[i] != '\0') /* count the character of the string */
-	{
-		i++;
-	}
-	return (i);
-}
-
-/**
- * _strcpy - copy arrays
- * @src: source string
- * @dest: destination of the string
- * Return: dest.
- */
-char *_strcpy(char *dest, char *src)
-{
-	int i = 0;
-
-	while (src[i] != '\0')
-	{
-		dest[i] = src[i];
-		i++;
-	}
-	dest[i] = '\0';
-	return (dest);
-}
-
-/**
- * _strdup - a function that returns a pointer to a newly allocated space
- * which contains a copy of the string given as a parameter
- * @str: array of elements
- * Return: a pointer
- */
 char *_strdup(char *str)
 {
 	char *dest;
-	unsigned int size;
+	int i, j;
 
 	if (str == 0)
-	{
 		return (NULL);
-	}
-	size = _strlen(str) + 1;
-	dest = (char *) malloc(size * sizeof(char));
-
-	if (dest = 0)
-	{
+	for (i = 0; str[i]; i++)
+		j++;
+	dest = malloc(sizeof(char) * (j + 1));
+	if (dest == 0)
 		return (NULL);
+	for (i = 0; str[i]; i++)
+	{
+		dest[i] = str[i];
+		j++;
 	}
-	_strcpy(dest, str);
+	dest[j] = '\0';
 	return (dest);
 }
