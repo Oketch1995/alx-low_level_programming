cc4ec707eca430bfaa9b5518ea22b3ff20a185de
Oketch1995 <velmaatieno4009@gmail.com>
2022-10-12 04:25:09 -0700

    task 0

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,6 +1,6 @@
-#ifndef 0-object_like_macro.h
-#define 0-object_like_macro.h
+#ifndef _0_OBJECT_LIKE_MACRO_H
+#define _0_OBJECT_LIKE_MACRO_H.h
 
 #define SIZE 1024
 
-#endif /* 0-object_like_macro.h */
+#endif /* _0_OBJECT_LIKE_MACRO_H */
