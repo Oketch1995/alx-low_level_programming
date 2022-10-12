0705678783557a1614882d770a389278e9bfdc21
Oketch1995 <velmaatieno4009@gmail.com>
2022-10-12 04:51:06 -0700

    task 2

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+
+/**
+ * main - write a program that prints the name of the
+ * file it was compiled from, followed by a new line
+ *
+ * Return: 0 success
+ */
+int main(void)
+{
+	printf("%s\n", _FILE_);
+}
