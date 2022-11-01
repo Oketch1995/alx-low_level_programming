066c7ce558443747322ad5ab01ac864c7f05a16a
Oketch1995 <velmaatieno4009@gmail.com>
2022-10-31 19:45:57 -0700

    task sero

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,44 +1,37 @@
 #include "main.h"
 
 /**
-* check_str - check if the string contains 1 and 0
-* @b: string to be checked
-* Return: 1 if string is validated or else 0
-*/
-
-int check_str(const char *b)
-{
-	if (b == NULL)
-		return (0);
-	while (*b)
-	{
-		if (*b != '1' && *b != '0')
-			return (0);
-		b++;
-	}
-	return (1);
-}
-/**
-* binary_to_uint - a function that converts a binary number to an unsigned int
-* @b: pointer to the strings of 0 and 1 chars
-* Return: the converted number, or 0 if there is one or
-* more chars in the string b that is not 0 or 1 or b is NULL
-*/
-
+ * binary_to_uint - converts a binary number to an
+ * unsigned int.
+ * @b: binary.
+ *
+ * Return: unsigned int.
+ */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int d = 0;
-	int len = 0, base = 1;
+	unsigned int ui;
+	int len, base_two;
 
-	if (!check_str(b))
+	if (!b)
 		return (0);
-	while (b[len] != '\0')
-		len++;
-	while (len)
+
+	ui = 0;
+
+	for (len = 0; b[len] != '\0'; len++)
+		;
+
+	for (len--, base_two = 1; len >= 0; len--, base_two *= 2)
 	{
-		d += ((b[len - 1] - '0') * base);
-		base *= 2;
-		len--;
+		if (b[len] != '0' && b[len] != '1')
+		{
+			return (0);
+		}
+
+		if (b[len] & 1)
+		{
+			ui += base_two;
+		}
 	}
-	return (d);
+
+	return (ui);
 }
