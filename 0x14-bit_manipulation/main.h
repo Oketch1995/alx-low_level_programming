12a5ee4860324c1938f22d9e86db518d79308d75
Oketch1995 <velmaatieno4009@gmail.com>
2022-10-31 19:16:19 -0700

    task 0

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,8 +1,15 @@
 #ifndef MAIN_H
 #define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 /**
  * all the prototypes
+ *
  */
+
 int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
