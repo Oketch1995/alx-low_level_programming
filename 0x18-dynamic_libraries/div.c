5fae1499e66839651eeb0245c43fdc87f0719eec
Oketch Velma <111089084+Oketch1995@users.noreply.github.com>
2022-12-13 22:57:35 +0300

    Update div.c

---
+++ 0x18-dynamic_libraries/div.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * div - product of two integers
+ * @a: The first integer 
+ * @b: The second integer 
+ *
+ * Return: The result of the product
+ */
+int add(int a, int b)
+{
+return (a / b);
+}
