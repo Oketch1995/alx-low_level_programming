61fb058cf34459f878dec18b249bc2b1f85128a5
Oketch Velma <111089084+Oketch1995@users.noreply.github.com>
2022-12-13 22:55:01 +0300

    Update mul.c

---
+++ 0x18-dynamic_libraries/mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * mul - product of two integers
+ * @a: The first integer to be multiplied
+ * @b: The second integer to be multiplied
+ *
+ * Return: The result of the multiplication
+ */
+int add(int a, int b)
+{
+return (a * b);
+}
