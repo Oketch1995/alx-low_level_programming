d4c3658a2563f842c99bd0ce468f8c667523b3ca
Oketch Velma <111089084+Oketch1995@users.noreply.github.com>
2022-12-13 22:53:00 +0300

    Update sub.c

---
+++ 0x18-dynamic_libraries/sub.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * sub - difference btwn two integers
+ * @a: The first integer to be subtracted
+ * @b: The second integer to be subtracted
+ *
+ * Return: The result of the subtraction
+ */
+int add(int a, int b)
+{
+return (a - b);
+}
