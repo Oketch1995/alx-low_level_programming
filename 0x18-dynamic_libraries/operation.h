ddc91bca0db622053165019faf6262617d3fab85
Oketch Velma <111089084+Oketch1995@users.noreply.github.com>
2022-12-13 23:05:06 +0300

    Update operation.h

---
+++ 0x18-dynamic_libraries/operation.h
@@ -0,0 +1,11 @@
+#ifndef _OPERATIONS_H_
+#define _OPERATIONS_H_
+
+int add(int a, int b);
+int mul(int a, int b);
+int div(int a, int b);
+int rem(int a, int b);
+int sub( int a, int b);
+
+
+#endif    /* OPERATION_H_ */
