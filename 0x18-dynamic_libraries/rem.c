a45fdee90408b25a20304b25fcd5e0a29095570a
Oketch Velma <111089084+Oketch1995@users.noreply.github.com>
2022-12-13 22:49:28 +0300

    Update rem.c

---
+++ 0x18-dynamic_libraries/rem.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * sub - subtract two integers
+ * @a: The first integer to be subtracted
+ * @b: The second integer to be subtracted
+ *
+ * Return: The result of the subtraction
+ */
+int sub(int a, int b)
+{
+return (a - b);
+}
